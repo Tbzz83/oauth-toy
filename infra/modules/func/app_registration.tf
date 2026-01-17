181fbebccd0b2bdddcf91b996ccbb0e27bd5d525
tbzz83 <tbzz83@gmail.com>
2026-01-17 13:26:43 -0500

    Adding app roles. Blocked on assigning

--- infra/modules/func/app_registration.tf
+++ infra/modules/func/app_registration.tf
@@ -3,12 +3,12 @@ locals {
 }
 
 resource "azuread_application_registration" "app" {
-  display_name                       = "OIDC Toy"
-  description                        = "A toy project demonstrating OIDC and Oauth2.0 authn/authz"
-  sign_in_audience                   = "AzureADMyOrg"
-  implicit_id_token_issuance_enabled = true
+  display_name                           = "OIDC Toy"
+  description                            = "A toy project demonstrating OIDC and Oauth2.0 authn/authz"
+  sign_in_audience                       = "AzureADMyOrg"
+  implicit_id_token_issuance_enabled     = true
   implicit_access_token_issuance_enabled = true
-  homepage_url                       = "https://${local.func_app_default_hostname}/api/home"
+  homepage_url                           = "https://${local.func_app_default_hostname}/api/home"
   #logout_url            = "https://app.example.com/logout"
   #marketing_url         = "https://example.com/"
   #privacy_statement_url = "https://example.com/privacy"
@@ -24,6 +24,33 @@ resource "azuread_application_redirect_uris" "redirect" {
     "https://${local.func_app_default_hostname}/api/auth-response",
     "http://localhost:7071/api/auth-response"
   ]
+}
+
+resource "random_uuid" "example_administrator" {}
+
+# Can create a basic Admin role for our app
+resource "azuread_application_app_role" "example_administer" {
+  application_id = azuread_application_registration.app.id
+  role_id        = random_uuid.example_administrator.id
+
+  allowed_member_types = ["User"]
+  description          = "Admin roles have the ability to view all sessions and other data"
+  display_name         = "Administer"
+  value                = "admin"
+}
+
+data "azuread_client_config" "current" {
+}
+
+# 3. Create the Service Principal (The missing link)
+resource "azuread_service_principal" "app_sp" {
+  client_id = azuread_application_registration.app.client_id
+}
+
+resource "azuread_app_role_assignment" "current_admin" {
+  app_role_id         = azuread_application_app_role.example_administer.role_id
+  principal_object_id = data.azuread_client_config.current.object_id
+  resource_object_id  = azuread_service_principal.app_sp.object_id
 }
 
 resource "azuread_application_password" "pw" {
